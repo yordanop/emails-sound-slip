008411c74e7c7db9c7ef88c30a3cfc6bbf2e6857
Eynar <yordanopv@gmail.com>
2024-06-16 19:43:44 -0600

    create seeds.sql file

--- Main/index.js
+++ Main/index.js
@@ -4,24 +4,52 @@ const inquirer = require("inquirer");
 const questions = [
     {
         type: "list",
-        name: "options", 
+        name: "action", 
         message: "What would you like to do?",
         choices: ['View all departments', 'View all Roles', 'View all employees',  'Add department', 'Add role', 'Add an employee', 'Update an employee role', 'Quit']
     }
 ];
 
 
-    // Create a function to initialize app
+// Create a function to initialize app
 function init() {
     
-    
     // First inquirer will make the questions to perform the selected option
-    inquirer.prompt(questions).then((answer) => {
+    inquirer.prompt(questions).then((options) => {
 
-        let keepAsking = true ? answer.options !== 'Quit' : false;
+        let answer = options.action
+
+        let keepAsking = true ? answer !== 'Quit' : false;
 
         if(keepAsking){
-            console.log(answer.options);
+
+            switch(answer){
+                case "View all departments":
+                    
+                    break;
+                case "View all Roles":
+                    
+                    break;
+                case "View all employees":
+                    
+                    break;
+                case "Add department":
+                    
+                    break;
+                case "Add role":
+                    
+                    break;
+                case "Add an employee":
+                    
+                    break;
+                case "Update an employee role":
+                    
+                    break;
+                default:
+                    console.log("That's what you want it? You kill this app :(")
+                    break;
+            }
+
             init();
         }
         
