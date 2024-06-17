c2674bc709b460f200b013bb03fbd71d351fa7e9
Eynar <yordanopv@gmail.com>
2024-06-16 19:17:43 -0600

    set loop logic for questions

--- Main/index.js
+++ Main/index.js
@@ -6,20 +6,25 @@ const questions = [
         type: "list",
         name: "options", 
         message: "What would you like to do?",
-        choices: ['View all departments', 'View all Roles', 'View all employees',  'Add department', 'Add role', 'Add an employee', 'Update an employee role']
+        choices: ['View all departments', 'View all Roles', 'View all employees',  'Add department', 'Add role', 'Add an employee', 'Update an employee role', 'Quit']
     }
 ];
 
 
     // Create a function to initialize app
 function init() {
-
-        // First inquirer will make the questions to perform the selected option
-    inquirer.prompt(questions).then(({options}) => {
-        console.log(options);
-
     
     
+    // First inquirer will make the questions to perform the selected option
+    inquirer.prompt(questions).then((answer) => {
+
+        let keepAsking = true ? answer.options !== 'Quit' : false;
+
+        if(keepAsking){
+            console.log(answer.options);
+            init();
+        }
+        
     });
 
 };
