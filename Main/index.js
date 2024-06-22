31b51fe92bfe0d18b8eb4a501ddd00aeb61c0c9d
Eynar <yordanopv@gmail.com>
2024-06-22 14:07:44 -0600

    query to see all employees

--- Main/index.js
+++ Main/index.js
@@ -17,6 +17,34 @@ const mainQuestions = [
   }
 ];
 
+const addDepartmentQuestions = [
+  {
+    type: "input", 
+    name: "departmentName",
+    message: "What is the name of the department?"
+  }
+]
+
+const addRoleQuestions = [
+  {
+    type: "input", 
+    name: "roleName",
+    message: "What is the name of the Role?"
+  },
+  {
+    type: "input", 
+    name: "roleSalary",
+    message: "What is the salary of the role?"
+  },
+  {
+    type: "input", 
+    name: "roleSalary",
+    message: "Which department does the role belong to?"
+  }
+]
+
+
+
 function renderTable(tableRows){
   console.log('\n')
   console.table(tableRows);
@@ -45,12 +73,45 @@ function init() {
                 queryFromAction = 'SELECT role.id, role.title, department.name as department, role.salary FROM role JOIN department ON role.department_id = department.id';
                 break;
 
+                
               case "View all employees":
+                queryFromAction =  `With 
+                department_roles AS (
+                SELECT 
+                  role.id AS role_id, 
+                  title, 
+                  salary, 
+                  department.name AS department 
+                FROM 
+                  role 
+                JOIN 
+                  department 
+                ON 
+                  role.department_id = department.id), 
 
-                queryFromAction = 'SELECT * FROM employee JOIN role ON employee.role_id = role.id;';
+                manager_table as (
+                SELECT 
+                  id as manager_id, 
+                  CONCAT(first_name, ' ', last_name) AS manager 
+                FROM 
+                  employee), 
+
+                employee_role as (
+                SELECT 
+                  * 
+                FROM 
+                  employee 
+                JOIN 
+                  department_roles
+                ON 
+                  employee.role_id = department_roles.role_id) 
+                  
+                SELECT id, first_name, last_name, title, department, salary, manager FROM employee_role LEFT JOIN manager_table ON employee_role.manager_id = manager_table.manager_id;`;
+                
                 break;
                 
               case "Add department":
+                inquirer.prompt(mainQuestions).then((options) => {});
                   
                   break;
               case "Add role":
