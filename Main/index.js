c966049cc076006778cc140b5a5c22672f6fe70a
Eynar <yordanopv@gmail.com>
2024-06-30 17:36:35 -0600

    added add role actions

--- Main/index.js
+++ Main/index.js
@@ -6,6 +6,23 @@ const { Console } = require('console');
 const { pool, app }= require('./server.js');
 const { resolve } = require("path");
 
+// Function to retrieve departments from the departments table
+const getListDepartments = async () => {
+  try {
+    const result = await pool.query('SELECT id, name FROM department');
+    const departments = result.rows;
+    const departmentChoices = departments.map(dept => ({
+      name: dept.name,
+      value: dept.id,
+    }));
+
+    return departmentChoices;
+  } catch (err) {
+    console.error('Error retrieving departments:', err.stack);
+    return [];
+  }
+};
+
 
 // questions for the first section
 const mainQuestions = [
@@ -16,12 +33,39 @@ const mainQuestions = [
       choices: ['View all departments', 'View all Roles', 'View all employees',  'Add department', 'Add role', 'Add an employee', 'Update an employee role', 'Quit']
   },
   {
+    // if "add department" option selected, make this quesiton
     when: input => {
       return input.action == 'Add department'
     },
     type: "input", 
     name: "departmentName",
     message: "What is the name of the department?"
+  },
+  // if "add role" option selected, make these quesitons
+  {
+    when: input => {
+      return input.action == 'Add role'
+    },
+    type: "input", 
+    name: "roleName",
+    message: "What is the name of the role?"
+  },
+  {
+    when: input => {
+      return input.action == 'Add role'
+    },
+    type: "input", 
+    name: "roleSalary",
+    message: "What is the salary of the role?"
+  },
+  {
+    when: input => {
+      return input.action == 'Add role'
+    },
+    type: "list", 
+    name: "roleDepartment",
+    message: "Which department does the role belong to?",
+    choices: getListDepartments
   }
 ];
 
@@ -35,11 +79,13 @@ function renderTable(tableRows){
 // Create a function to initialize app
 function init() {
     
-  // First inquirer will make the questions to perform the selected option
+  // First inquirer will make the questions to perform a query depending on the selected option
   inquirer.prompt(mainQuestions).then((options) => {
+    
 
       let answer = options.action
       let queryFromAction = '';
+      let params = []
       let keepAsking = answer !== 'Quit';
 
       if(keepAsking){
@@ -120,20 +166,30 @@ function init() {
                 
               case "Add department":
 
-                
-                  const sql = `INSERT INTO department (name) VALUES ($1)`;
-                  const params = [options.departmentName];
 
-                  pool.query(sql, params, (err, result) => {
-                    if (err) {
-                      console.log(err)
-                      return;
-                    }
+                queryFromAction = `INSERT INTO department (name) VALUES ($1)`;
+                params = [options.departmentName];
+
+                pool.query(queryFromAction, params, (err, result) => {
+                  if (err) {
+                    console.log(err)
+                    return;
+                  }
               
-                  });
+                });
                   
                   break;
               case "Add role":
+              queryFromAction = 'INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)';
+              params = [options.roleName, options.roleSalary, options.roleDepartment];
+
+              pool.query(queryFromAction, params, (err, result) => {
+                if (err) {
+                  console.log(err)
+                  return;
+                }
+            
+              });
                   
                   break;
               case "Add an employee":
